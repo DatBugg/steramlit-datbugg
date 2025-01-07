5a6451eb5765710853692935055702e11279d7a4
Datbugg <gonzagalante@gmail.com>
2025-01-07 14:36:05 -0300

    2025

---
+++ courses/cloud/Google/e1_google_introduction.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def e1_google_introduction():
+    # Page configuration (title and description)
+    st.title("🚀 Google Cloud introduction")
---
+++ courses/utilities/util1/e1_util1.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def e1_util1():
+    # Page configuration (title and description)
+    st.title("🚀 Util 1 introduction")
